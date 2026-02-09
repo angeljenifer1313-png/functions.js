66eb24169017349ce6feabd61d1c05f6cbcd3a77
angeljenifer1313-png <angeljenifer1313@gmail.com>
2026-02-09 09:08:22 -0800

    Add files via upload

---
+++ push,pop.js
@@ -0,0 +1,29 @@
+let numbers = [10, 20, 30];
+
+console.log(numbers); 
+
+// push
+numbers.push(40);
+console.log(numbers); 
+
+// pop
+numbers.pop();
+console.log(numbers); 
+
+
+// unshift
+numbers.unshift(5);
+console.log(numbers); 
+
+
+// shift
+numbers.shift();
+console.log(numbers); 
+
+
+//loop
+let numbers = [10, 20, 30, 40, 50];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
