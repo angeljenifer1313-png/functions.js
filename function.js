7a1ad2ec09c24902a9bec3ab67cd9bf494017c05
angeljenifer1313-png <angeljenifer1313@gmail.com>
2026-02-09 09:09:03 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,30 @@
+//add two numbers
+
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+
+let result = addTwoNumbers(2, );
+console.log(result); 
+
+//multiply two numbers
+const multiply = function(a, b) {
+  return a * b;
+};
+let result = multiply(5, 2);
+console.log(result); 
+
+//arrow subtract
+const subtract = (a, b) => a - b;
+
+let result = subtract(10, 4);
+console.log(result); 
+
+//functions with parameters
+function squareNumber(num) {
+  return num * num;
+}
+
+let result = squareNumber(5);
+console.log(result); 
